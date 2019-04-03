2e08011a77f1dcba878a5e15416d7132eb32a34b
Khalildubruh <fortechsignup@gmail.com>
2019-04-03 10:50:03 -0700

    What I did to this code.
    I changed the ball to dog and tried to add another shape (a rectangle) that called bone. It didnt work.

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -4,6 +4,8 @@ let anotherDog;
 
 let c;
 
+let d;
+let anotherBone;
 
 function setup() {
   createCanvas(800, 400);
@@ -11,6 +13,8 @@ function setup() {
   anotherDog = new Dog(200,20,"green");
   c = new Dog(69, 69, "purple");
   anotherDog = new Dog(10,200,"blue");
+  d = new Bone(44, 44, "black");
+  anotherBone = new Bone(255, 10, "orange");
 }
 
 
@@ -18,6 +22,10 @@ function draw(){
 	background(220);
     b.drawDog(); //draw the Dog called b (go look in the Dog class for the drawDog function)
     b.moveDog(); //move the Dog called b (go look in the Dog class for the moveDog function)
+    b.drawBone();
+    b.moveBone();
+    anotherBone.drawBone();
+    anotherBone.drawBone();
     anotherDog.drawDog();
     anotherDog.moveDog();
 }
@@ -33,7 +41,7 @@ class bone {
 	drawBone(){  // draw a Dog on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    ellipse(this.x, this.y, 100)
+		    ellipse(this.x, this.y, 10, 20)
 	}
 	moveBone(){ //update the location of the Dog, so it moves across the screen
 		this.x = this.x+2;
